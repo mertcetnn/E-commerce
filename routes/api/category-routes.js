a42aedebe29775e8da8029753140700043c2bfd0
MertCetin <mert.cetnn@gmail.com>
2022-10-23 23:28:26 -0400

    checked class documetions and made adjustments

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -50,15 +50,59 @@ router.get("/:id", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-  // create a new category
+  Category.create({
+    id: req.body.id,
+    category_name: req.body.category_name,
+  })
+    .then((dbCategoryData) => res.json(dbCategoryData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put("/:id", (req, res) => {
-  // update a category by its `id` value
+  Category.update(
+    {
+      id: req.body.title,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((dbCategoryData) => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-const express = require('express');
-const routes = require('./routes');
-// import sequelize connection
+const express = require("express");
+const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
